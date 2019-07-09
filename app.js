714691f65744458703feb9b23bdffbefa34ea428
Tony <tony.n.flpa@gmail.com>
2019-07-09 14:48:59 -0400

    close

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const mysql      = require('mysql');
 
 const urlEncoded = bodyParser.urlencoded({extended: false})
-const db = mysql.createConnection({
+const dataBase = mysql.createConnection({
     host     : 'localhost',
     user     : 'admin',
     password : 'student',
@@ -11,6 +11,7 @@ const db = mysql.createConnection({
   });
 
 const postData = []
+const postDate = []
 // Server
 const app = express();
 
@@ -25,72 +26,68 @@ app.use(express.static('./public'));
 app.get('/', (req, res) => {
     res.render('home.ejs');
 });
+
 app.get('/logs', (req, res) => {
     res.render('logs', {blogPosts: postData});
 });
+
 app.get('/caplog1', (req, res) => {
     res.render('cap1');
 });
+
+// app.get('/caplog2', (req, res) => {
+//     res.render('cap2', {blogPosts: postData, date: postDate});
+// });
+
 app.get('/caplog2', (req, res) => {
-    res.render('cap2');
+    let sql = 'SELECT * FROM LogEntry WHERE PAGE=2';
+    dataBase.query(sql, function (err, results) {
+        if (err) throw err;
+        // rendering tasks view and passing taskToDo data
+        res.render('cap2', {blogPosts: postData, date: postDate});
+      });
 });
+
 app.get('/caplog3', (req, res) => {
     res.render('cap3');
 });
+
 app.get('/caplog4', (req, res) => {
     res.render('cap4');
 });
+
 app.get('/caplog5', (req, res) => {
     res.render('cap5');
 });
+
 app.get('/caplog6', (req, res) => {
     res.render('cap6');
 });
+
 app.get('/caplog7', (req, res) => {
     res.render('cap7');
 });
+
 app.get('/caplog8', (req, res) => {
     res.render('cap8');
 });
+
 app.get('/caplog9', (req, res) => {
     res.render('cap9');
 });
-app.get('/caplog10', (req, res) => {
-    res.render('cap10');
-});
-app.get('/caplog11', (req, res) => {
-    res.render('cap11');
-});
-app.get('/caplog12', (req, res) => {
-    res.render('cap12');
-});
-app.get('/caplog13', (req, res) => {
-    res.render('cap13');
-});
-app.get('/caplog14', (req, res) => {
-    res.render('cap14');
-});
-app.get('/caplog15', (req, res) => {
-    res.render('cap15');
-});
-
 //POST Routes 
 app.post('/logs', urlEncoded, (req, res) => {
     let incomingEntry = {};
-    incomingEntry.logEntry = req.body.Title;
+    let theDate = {};
+    incomingEntry.logEntry = req.body.Body;
+    theDate.date = req.body.Title
     postData.push(incomingEntry)
+    postDate.push(theDate)
     res.redirect('/logs')
 });
 
-app.post("/addpost", (req, res) => {
-    let post = {title: "my first post", body: "hello today was a good day"}
-    let sql = 'INSERT INTO posts SET ?'
-    db.query(sql, post, (err, result) => {
-        if (err) throw err;
-        console.log(result);
-        res.send('first post added ...')
-    })
-})
+
+
 // Listening
 app.listen(3000, function(err){
     if (err)
